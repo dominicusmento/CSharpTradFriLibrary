dd14370b24964b79b4714e8a898d919d052f6ac9
tomidix <mario.mustapic13@gmail.com>
2018-05-11 02:24:38 +0200

    Develop (#10)
    * nuget package configured, downgraded on .net 4.5 cause of rpi, changed namespace, some minor functionalities added to group and deviice controllers..
    
    * Feature/some minor functionalities (#5)
    
    * documentation on existing functions added, groups example usage added
    
    * Group moods implemented, some code tweaks, device color implementation now works
    
    * SmartTask implemented
    
    * fixed issue #4 - Connect Method with invalid details doesnt throw
    
    * readme updated
    
    * readme updated with badges
    
    * fix issue #9, newer connection method using appSecret implemented

---
+++ TradFriLibrary/Models/TradFriAuth.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace Tomidix.CSharpTradFriLibrary.Models
+{
+    public class TradFriAuth
+    {
+        [JsonProperty("9091")]
+        public string PSK { get; set; }
+        [JsonProperty("9029")]
+        public string FirmwareVersion { get; set; }
+    }
+}
